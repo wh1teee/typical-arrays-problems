fdd9b6b719a5034b973349483df0f9a50cac815b
wh1teee <wh1teee1337lol@gmail.com>
2020-10-03 20:41:41 +0300

    add: start typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
+exports.min = function min(array) {
+    if (!Array.isArray(array) || array.length == 0) {
+        return 0
+    }
+    function compareNumeric(a, b) {
+        if (a > b) return 1;
+        if (a == b) return 0;
+        if (a < b) return -1;
+    }
 
-exports.min = function min (array) {
-  return 0;
+
+    return array.sort(compareNumeric).shift()
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!Array.isArray(array) || array.length == 0) {
+        return 0
+    }
+
+    function compareNumeric(a, b) {
+        if (a > b) return 1;
+        if (a == b) return 0;
+        if (a < b) return -1;
+    }
+
+
+    return array.sort(compareNumeric).pop()
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!Array.isArray(array) || array.length == 0) {
+        return 0
+    }
+
+    function compareNumeric(a, b) {
+        if (a > b) return 1;
+        if (a == b) return 0;
+        if (a < b) return -1;
+    }
+
+    let temp = array.reduce((sum, current) => {
+        return sum + current
+    })
+    return temp / array.length
 }
