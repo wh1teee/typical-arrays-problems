929aef9631d00a325a84cc4bab045156319fed26
wh1teee <hemp0@mail.ru>
2021-03-09 19:58:55 +0300

    refactor: some improvements

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 exports.min = function min (array) {
-    return (Array.isArray(array) && array.length) ? Math.min(...array) : 0;
+    return checkArrayValidation(array) ? Math.min(...array) : 0;
 };
 
 exports.max = function max (array) {
-    return (Array.isArray(array) && array.length) ? Math.max(...array) : 0;
+    return checkArrayValidation(array) ? Math.max(...array) : 0;
 
 };
 
 exports.avg = function avg (array) {
-    return (Array.isArray(array) && array.length) ? array.reduce((acc, curr) => acc + curr, 0) / array.length : 0;
+    return checkArrayValidation(array) ? array.reduce((acc, curr) => acc + curr, 0) / array.length : 0;
 };
+
+const checkArrayValidation = (array) => {
+    return Array.isArray(array) && array.length
+}
